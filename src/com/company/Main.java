bbefa64d94a59229c981c62adcf4d6b6cfd1ddeb
Rs_Durvesh <76864632+DurveshKumarPal@users.noreply.github.com>
2021-06-17 11:47:08 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner abc = new Scanner(System.in);
+
+        float a=39.493f;
+        System.out.println("Hellow world");
+        System.out.println(a);
+        System.out.println("Enter the number");
+        int k =abc.nextInt();
+    }
+}
