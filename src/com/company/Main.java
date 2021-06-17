8fb4827c3ce57654c75bce61ac35e5875568b9c6
Rs_Durvesh <76864632+DurveshKumarPal@users.noreply.github.com>
2021-06-17 12:01:48 +0530

    Sum of two numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,15 @@
-package com.company;
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number 1");
+        int num1 = sc.nextByte();
+        System.out.println("enter the number 2");
+        int num2 = sc.nextByte();
+        int sum = num1 + num2;
+        System.out.print("The sum of two no:");
+        System.out.println(sum);
 
-        Scanner abc = new Scanner(System.in);
-
-        float a=39.493f;
-        System.out.println("Hellow world");
-        System.out.println(a);
-        System.out.println("Enter the number");
-        int k =abc.nextInt();
     }
 }
